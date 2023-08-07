7e90e3a88c2fb8e153e1ebc3f47be433c5c8aa6e
AGS Lakpahana <agslakpahana@gmail.com>
2023-08-07 20:31:45 +0530

    demo video player

--- app/src/main/java/com/example/swipeablevdeos/MainActivity.kt
+++ app/src/main/java/com/example/swipeablevdeos/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.swipeablevdeos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MimeTypes
+import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
+import androidx.media3.ui.PlayerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var playerView: PlayerView
+    lateinit var player: ExoPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.item_video)
+        playerView = findViewById(R.id.playerView)
+
+        // Create an ExoPlayer instance
+        player = ExoPlayer.Builder(this).build()
+
+
+        val videoUri = "https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4";
+
+        // Build the media item.
+        val mediaItem = MediaItem.fromUri(videoUri)
+// Set the media item to be played.
+        player.setMediaItem(mediaItem)
+// Prepare the player.
+        player.prepare()
+// Start the playback.
+        player.play()
+
+
     }
 }
